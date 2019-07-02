e7b80c9b74c92be3f599b7911168a7b6367634eb
Charlie Wu <wu@fox.one>
2019-07-02 10:36:06 +0800

    MOD: NewMessengerWithSession

--- messenger/messenger.go
+++ messenger/messenger.go
@@ -1,6 +1,11 @@
 package messenger
 
-import "github.com/fox-one/mixin-sdk/mixin"
+import (
+	"crypto/x509"
+	"encoding/pem"
+
+	"github.com/fox-one/mixin-sdk/mixin"
+)
 
 // Messenger mixin messenger
 type Messenger struct {
@@ -14,4 +19,20 @@ func NewMessenger(user *mixin.User) *Messenger {
 		user,
 		NewBlazeClient(user.UserID, user.SessionID, user.SessionKey),
 	}
+}
+
+// NewMessengerWithSession new messenger
+func NewMessengerWithSession(userID, sessionID, sessionKey string) (*Messenger, error) {
+	user := &mixin.User{
+		UserID:    userID,
+		SessionID: sessionID,
+	}
+
+	block, _ := pem.Decode([]byte(sessionKey))
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	user.SetPrivateKey(privateKey)
+	return NewMessenger(user), nil
 }
