d7e28c37a4423bc658bf36a8b84c256365157b39
Charlie Wu <wu@fox.one>
2019-07-17 14:53:06 +0800

    MOD:

--- mixin/chain.go
+++ mixin/chain.go
@@ -1,16 +1,20 @@
 package mixin
 
-import "time"
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
 
 // Chain chain info
 type Chain struct {
-	ChainID              string    `json:"chain_id"`
-	IconURL              string    `json:"icon_url"`
-	Name                 string    `json:"name"`
-	Type                 string    `json:"type"`
-	WithdrawFee          string    `json:"withdrawal_fee"`
-	WithdrawPendingCount int       `json:"withdrawal_pending_count"`
-	WIthdrawTimestamp    time.Time `json:"withdrawal_timestamp"`
+	ChainID              string          `json:"chain_id"`
+	IconURL              string          `json:"icon_url"`
+	Name                 string          `json:"name"`
+	Type                 string          `json:"type"`
+	WithdrawFee          decimal.Decimal `json:"withdrawal_fee"`
+	WithdrawPendingCount int             `json:"withdrawal_pending_count"`
+	WithdrawTimestamp    time.Time       `json:"withdrawal_timestamp"`
 
 	DepositBlockHeight  int  `json:"deposit_block_height"`
 	ExternalBlockHeight int  `json:"external_block_height"`
