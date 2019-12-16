89867f8637d5abae27f5b35035a493313ba52f77
李国银 <guoyinl@gmail.com>
2019-12-16 19:09:49 +0800

    fix conversation (#6)
    * fix conversation
    
    * mod: _

--- conversation.go
+++ conversation.go
@@ -3,10 +3,27 @@ package mixin
 import (
 	"context"
 	"time"
+
+	"github.com/fox-one/mixin-sdk/utils"
+)
+
+const (
+	ConversationCategoryContact = "CONTACT"
+	ConversationCategoryGroup   = "GROUP"
+
+	ParticipantActionAdd    = "ADD"
+	ParticipantActionRemove = "REMOVE"
+	ParticipantActionJoin   = "JOIN"
+	ParticipantActionExit   = "EXIT"
+	ParticipantActionRole   = "ROLE"
+
+	ParticipantRoleAdmin  = "ADMIN"
+	ParticipantRoleMember = ""
 )
 
 // Participant conversation participant
 type Participant struct {
+	Action    string     `json:"action,omitempty"`
 	Type      string     `json:"type,omitempty"`
 	UserID    string     `json:"user_id,omitempty"`
 	Role      string     `json:"role,omitempty"`
@@ -28,35 +45,39 @@ type Conversation struct {
 	Participants []*Participant `json:"participants,omitempty"`
 }
 
-// CreateConversation crate conversation
-func (user *User) CreateConversation(ctx context.Context, category, conversationID, name, action, role, userID string, participants []*Participant) (*Conversation, error) {
-	params := map[string]interface{}{
-		"category": category,
-	}
-	if conversationID != "" {
-		params["conversation_id"] = conversationID
-	}
-	if name != "" {
-		params["name"] = name
-	}
-	if action != "" {
-		params["action"] = action
-	}
-	if role != "" {
-		params["role"] = role
-	}
-	if userID != "" {
-		params["user_id"] = userID
-	}
-	if len(participants) > 0 {
-		params["participants"] = participants
-	}
+type CreateConversationInput struct {
+	Category       string         `json:"category,omitempty"`
+	ConversationID string         `json:"conversation_id,omitempty"`
+	Name           string         `json:"name,omitempty"`
+	Participants   []*Participant `json:"participants,omitempty"`
+}
 
+// CreateConversation crate conversation
+func (user *User) CreateConversation(ctx context.Context, input *CreateConversationInput) (*Conversation, error) {
 	var conversation Conversation
-	if err := user.Request(ctx, "POST", "/conversations", params, &conversation); err != nil {
+	if err := user.Request(ctx, "POST", "/conversations", input, &conversation); err != nil {
 		return nil, err
 	}
 	return &conversation, nil
+}
+
+// CreateContactConversation create a conversation with a mixin messenger user
+func (user *User) CreateContactConversation(ctx context.Context, userID string) (*Conversation, error) {
+	return user.CreateConversation(ctx, &CreateConversationInput{
+		Category:       ConversationCategoryContact,
+		ConversationID: utils.UniqueConversationID(user.UserID, userID),
+		Participants:   []*Participant{{UserID: userID}},
+	})
+}
+
+// CreateGroupConversation create a group in mixin messenger with given participants
+func (user *User) CreateGroupConversation(ctx context.Context, conversationID, name string, participants []*Participant) (*Conversation, error) {
+	return user.CreateConversation(ctx, &CreateConversationInput{
+		Category:       ConversationCategoryGroup,
+		ConversationID: conversationID,
+		Name:           name,
+		Participants:   participants,
+	})
 }
 
 // ReadConversation read conversation
@@ -65,5 +86,6 @@ func (user *User) ReadConversation(ctx context.Context, conversationID string) (
 	if err := user.Request(ctx, "GET", "/conversations/"+conversationID, nil, &conversation); err != nil {
 		return nil, err
 	}
+
 	return &conversation, nil
 }
