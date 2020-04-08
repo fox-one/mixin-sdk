66acfcffaf1c448de383ed339340ed35ce6db336
yiplee <guoyinl@gmail.com>
2020-04-08 15:05:51 +0800

    add pay url

---
+++ url.go
@@ -0,0 +1,21 @@
+package mixin
+
+import (
+	"net/url"
+)
+
+func PayURL(assetID, traceID, opponentID, amount, memo string) string {
+	u := &url.URL{
+		Scheme: "mixin",
+		Host:   "pay",
+	}
+
+	q := u.Query()
+	q.Set("asset", assetID)
+	q.Set("trace", traceID)
+	q.Set("amount", amount)
+	q.Set("recipient", opponentID)
+	u.RawQuery = q.Encode()
+
+	return u.String()
+}
