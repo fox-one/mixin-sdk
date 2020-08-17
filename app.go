b42794e6a32fdf3c5f041b5037d7db47f62f4708
李国银 <guoyinl@gmail.com>
2020-08-17 18:44:17 +0800

    read & update apps (#20)

---
+++ app.go
@@ -0,0 +1,55 @@
+package mixin
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+type App struct {
+	UpdatedAt        time.Time `json:"updated_at,omitempty"`
+	AppID            string    `json:"app_id,omitempty"`
+	AppNumber        string    `json:"app_number,omitempty"`
+	RedirectURL      string    `json:"redirect_url,omitempty"`
+	HomeURL          string    `json:"home_url,omitempty"`
+	Name             string    `json:"name,omitempty"`
+	IconURL          string    `json:"icon_url,omitempty"`
+	Description      string    `json:"description,omitempty"`
+	Capabilities     []string  `json:"capabilities,omitempty"`
+	ResourcePatterns []string  `json:"resource_patterns,omitempty"`
+	Category         string    `json:"category,omitempty"`
+	CreatorID        string    `json:"creator_id,omitempty"`
+	AppSecret        string    `json:"app_secret,omitempty"`
+}
+
+func (user *User) ReadApp(ctx context.Context, appID string) (*App, error) {
+	var app App
+	uri := fmt.Sprintf("/apps/%s", appID)
+	if err := user.Request(ctx, "GET", uri, nil, &app); err != nil {
+		return nil, err
+	}
+
+	return &app, nil
+}
+
+type UpdateAppRequest struct {
+	RedirectURI      string   `json:"redirect_uri,omitempty"`
+	HomeURI          string   `json:"home_uri,omitempty"`
+	Name             string   `json:"name,omitempty"`
+	Description      string   `json:"description,omitempty"`
+	IconBase64       string   `json:"icon_base64,omitempty"`
+	SessionSecret    string   `json:"session_secret,omitempty"`
+	Category         string   `json:"category,omitempty"`
+	Capabilities     []string `json:"capabilities,omitempty"`
+	ResourcePatterns []string `json:"resource_patterns,omitempty"`
+}
+
+func (user *User) UpdateApp(ctx context.Context, appID string, req UpdateAppRequest) (*App, error) {
+	var app App
+	uri := fmt.Sprintf("/apps/%s", appID)
+	if err := user.Request(ctx, "POST", uri, req, &app); err != nil {
+		return nil, err
+	}
+
+	return &app, nil
+}
